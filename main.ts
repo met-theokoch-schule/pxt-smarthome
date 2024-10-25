5869a111493b2d86c697dfbce8082d12317d4a85
met-theokoch-schule <91288462+met-theokoch-schule@users.noreply.github.com>
2024-10-25 22:29:59 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -37,7 +37,7 @@ namespace smarthome {
 
     const MPR121_TOUCH_SENSOR_TOUCHED_ID = 2148
     const MPR121_TOUCH_SENSOR_RELEASED_ID = 2149
-    const PRESENCE_DETECTED_ID = 2147
+    //const PRESENCE_DETECTED_ID = 2147
 
     /**
      * Initialize the touch controller.
@@ -123,7 +123,7 @@ namespace smarthome {
 
     function detectAndNotifyTouchEvents() {
         let previousTouchStatus = 0
-        let previousPresenceStatus = false
+        //let previousPresenceStatus = false
         
         while (true) {
             const touchStatus = mpr121.readTouchStatus(MPR121_ADDRESS)
@@ -150,27 +150,27 @@ namespace smarthome {
 
             previousTouchStatus = touchStatus
 
-            if (previousPresenceStatus === false && pins.digitalReadPin(DigitalPin.C9) === 0) {
+            /*if (previousPresenceStatus === false && pins.digitalReadPin(DigitalPin.C9) === 0) {
                 control.raiseEvent(PRESENCE_DETECTED_ID, true)
                 previousPresenceStatus = true
             }
             if (previousPresenceStatus === true && pins.digitalReadPin(DigitalPin.C9) === 1) {
                 previousPresenceStatus = false
-            }
+            }*/
             
             basic.pause(TOUCH_STATUS_PAUSE_BETWEEN_READ)
         }
     }
 
     // Änderungen gemacht :(
-    //% blockId=smarthome_presence_detected
-    //% block="wenn Präsenz gemeldet"
-    //% weight=65
-    export function onPresenceDetected(handler: () => void) {
+    // blockId=smarthome_presence_detected
+    // block="wenn Präsenz gemeldet"
+    // weight=65
+    /*export function onPresenceDetected(handler: () => void) {
         control.onEvent(PRESENCE_DETECTED_ID, status, () => {
             handler()
         })
-    }
+    }*/
 
     /**
      * Mache etwas, wenn ein spezieller Sensor berührt wird.
